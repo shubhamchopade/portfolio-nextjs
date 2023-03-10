6a690744478d509034c96f735ddbe924948b8af3
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2023-03-10 01:38:09 -0500

    remove event listener

--- components/ScrollText.tsx
+++ components/ScrollText.tsx
@@ -2,73 +2,28 @@ import { useEffect, useState } from "react";
 import { useScroll } from "framer-motion";
 import styled from "styled-components";
 import { Button } from "./HireButton";
-import Lottie from "lottie-react";
-import dynamic from "next/dynamic";
-import thankyouAnimation from "./assets/82521-thank-you.json";
-import { useLottie, useLottieInteractivity } from "lottie-react";
-import useWindowSize from "react-use/lib/useWindowSize";
-import Confetti from "react-confetti";
-import Image from "next/image";
-
-const style = {
-  height: 300,
-  border: 3,
-  borderStyle: "solid",
-  borderRadius: 7,
-};
-
-const options = {
-  animationData: thankyouAnimation,
-};
-
-const Example = () => {
-  const lottieObj = useLottie(options, style);
-
-  const Animation = useLottieInteractivity({
-    lottieObj,
-    mode: "cursor",
-    actions: [
-      {
-        position: { x: [0, 1], y: [-1, 2] },
-        type: "seek",
-        frames: [0, 179],
-      },
-      {
-        position: { x: -1, y: -1 },
-        type: "stop",
-        frames: [0],
-      },
-    ],
-  });
-  return Animation;
-};
 
 export const ScrollText = () => {
   const [isScrolled, setIsScrolled] = useState(0);
   const { scrollY } = useScroll();
-  const { width, height } = useWindowSize();
 
   useEffect(() => {
-    const scrollEvent = () =>
-      window.addEventListener("scroll", (e) => {
-        let winScroll =
-          document.body.scrollTop || document.documentElement.scrollTop;
+    window.addEventListener("scroll", (e) => {
+      let winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
 
-        let height =
-          document.documentElement.scrollHeight -
-          document.documentElement.clientHeight;
+      let height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
 
-        let scrolled = (winScroll / height) * 100;
+      let scrolled = (winScroll / height) * 100;
 
-        setIsScrolled(scrolled);
-      });
-
-    () => window.removeEventListener("scroll", scrollEvent);
+      setIsScrolled(scrolled);
+    });
   }, [scrollY]);
 
   return (
     <Wrapper>
-      <Confetti numberOfPieces={100} width={width} height={height} />
       <Container>
         <SVG
           width="1020"
@@ -77,7 +32,7 @@ export const ScrollText = () => {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <Text x="10%" y="50%" text-anchor="middle">
+          <Text x="10%" y="50%" text-anchor="middle" isScrolled={isScrolled}>
             GET YOUR WORK DONE
           </Text>
         </SVG>
@@ -108,12 +63,11 @@ const SVG = styled.svg`
 `;
 
 interface TextProps {
-  str: number;
+  isScrolled: number;
 }
 
 const Text = styled.text.attrs((props: TextProps) => ({
-  strokeOpacity: props.str + "%",
-  fillOpacity: props.str > 90 ? props.str + "%" : 0,
+  fillOpacity: props.isScrolled > 90 ? "100%" : "0%",
 }))`
   font-size: 4rem;
   font-weight: bold;
